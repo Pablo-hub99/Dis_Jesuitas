112f5f2abadc86b22be0a012d72e16b2b9f3e078
Pablo saenz <pablosnf.99@gmail.com>
2020-10-08 13:32:24 +0200

    Initial commit

---
+++ app/src/main/java/jesuitas/dam/dis_jesuitas/MainActivity.java
@@ -0,0 +1,22 @@
+package jesuitas.dam.dis_jesuitas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this,R.string.like,Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+}
